a6404b5ab2a735c0cb8cc50b3cbc413185d84fa4
caitlinoma11ey <93583826+caitlinoma11ey@users.noreply.github.com>
2024-01-10 18:07:49 +1100

    Initial commit - Set up navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const navigationLinks = [
+    { key: 0, label: "Home", location: "/" },
+    { key: 1, label: "About", location: "/" },
+    { key: 2, label: "Resume", location: "/" },
+  ];
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <p>Caitlin O'Malley</p>
+            <ul className="navbar-links">
+                {navigationLinks.map((link) => (
+                    <li key={link.key}>
+                        <a href={link.location} className="navbar-link">
+                            {link.label}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
